d0d5e728875ae0591a675067e43501a857aeec23
Peeduli <opilane@HP-CZC6297B8W.tthk.ee>
2021-10-20 13:16:55 +0300

    Initial

---
+++ src/ee/tthk/tarpe19/peeduli/GameOfLife.java
@@ -0,0 +1,5 @@
+package ee.tthk.tarpe19.peeduli;
+
+public class GameOfLife {
+
+}
---
+++ src/ee/tthk/tarpe19/peeduli/GameOfLifeTester.java
@@ -0,0 +1,14 @@
+package ee.tthk.tarpe19.peeduli;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameOfLifeTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
