a0a382bf1226b4d604fba7a956abead6551e15b6
opilane <opilane@HP-CZC6297B8W.tthk.ee>
2021-10-27 13:49:32 +0300

    Req2 - assert string with three dots -pass

--- src/ee/tthk/tarpe19/peeduli/IpValidator.java
+++ src/ee/tthk/tarpe19/peeduli/IpValidator.java
@@ -3,10 +3,14 @@ package ee.tthk.tarpe19.peeduli;
 public class IpValidator {
 
 	public boolean validateIPv4Address(String ipString) {
-		if(ipString == "") {
-			return false;
+		if(hasThreeDots(ipString)) {
+			return true;
 		}
 		return false;
+	}
+
+	private boolean hasThreeDots(String ipString) {
+		return ipString.chars().filter(c-> c == '.').count()==3;
 	}
 
 }
