3498c344145dc8adc819f3a7dc0b792571ce1f58
opilane <opilane@HP-CZC6297B8W.tthk.ee>
2021-11-01 08:47:43 +0200

    Refactor tests

--- src/ee/tthk/tarpe19/peeduli/IpValidatorTests.java
+++ src/ee/tthk/tarpe19/peeduli/IpValidatorTests.java
@@ -5,8 +5,11 @@ import org.junit.*;
 
 public class IpValidatorTests {
 
+	private IpValidator validator;
+	
 	@Before
 	public void setUp() throws Exception {
+		validator = new IpValidator();
 	}
 	
 	@After
@@ -16,39 +19,34 @@ public class IpValidatorTests {
 
 	@Test
 	public void ShouldReturn_False_GivenEmptyStringParameter() {
-		IpValidator validator = new IpValidator();
 		assertFalse(validator.validateIPv4Address(""));
 	}
 	
 	@Test
 	public void ShouldBe_True_GivenStringWithThreeDots() {
-		IpValidator validator = new IpValidator();
 		assertTrue(validator.validateIPv4Address("1.2.3.1"));
 	}
 	
 	@Test
 	public void ShouldReturnFourNumbers_GivenStringWithThreeDots() {
-		IpValidator validator = new IpValidator();
 		int[] expected = {1,2,3,4};
 		assertArrayEquals(expected, validator.getNumbers("1.2.3.4"));
 	}
 	
 	@Test
 	public void ShouldBe_False_GivenStringWithThreeNumbersAndDots() {
-		IpValidator validator = new IpValidator();
 		assertFalse(validator.validateIPv4Address("1.2.3."));
 	}
 	
 	@Test
 	public void ShouldBe_False_GivenNumbersEndingWith0() {
-		IpValidator validator = new IpValidator();
 		assertFalse(validator.validateIPv4Address("1.2.3.0"));
 	}
 	
 	@Test
 	public void ShouldBe_False_GivenNumbersEndingWith255() {
-		IpValidator validator = new IpValidator();
 		assertFalse(validator.validateIPv4Address("1.2.3.255"));
 	}
+
 
 }
